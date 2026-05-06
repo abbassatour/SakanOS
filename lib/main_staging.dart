e4aa752b9db356bc8cef6b0acb62265abe1054fc
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-05-06 12:51:21 +0300

    PRODUCTION DONE

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,8 +1,30 @@
-//main_development.dart
+// lib/main_development.dart
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart'; 
 import 'package:our_home_erp_app/app/app.dart';
 import 'package:our_home_erp_app/bootstrap.dart';
 
-void main() {
-  // نقوم بتمرير الـ erpRepository إلى التطبيق الأساسي (App)
+void main() async {
+  // 1. التأكد من تهيئة بيئة فلاتر والنافذة أولاً
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  // 2. إعدادات النافذة الافتراضية
+  WindowOptions windowOptions = const WindowOptions(
+    title: 'نظام بيتنا العقاري', 
+    center: true,
+    minimumSize: Size(800, 600), // يفضل وضع حجم أدنى لكي لا يصغر الموظف الشاشة جداً وتختفي الأزرار
+  );
+  
+  // 3. التحكم بالنافذة قبل إظهارها للمستخدم
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+    
+    // 🌟 السطر السحري الجديد: تكبير النافذة لتأخذ كامل الشاشة (Maximized) تلقائياً
+    await windowManager.maximize(); 
+  });
+
+  // 4. تشغيل التطبيق
   bootstrap((erpRepository) => App(erpRepository: erpRepository));
 }
