8c68bc9826542836f6f17e7ddf909fe6f28ac385
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-05-11 13:10:19 +0300

    cleaning

--- lib/dashboard/cubit/dashboard_cubit.dart
+++ lib/dashboard/cubit/dashboard_cubit.dart
@@ -2,7 +2,6 @@
 import 'package:bloc/bloc.dart';
 
 class DashboardCubit extends Cubit<int> {
-  // نبدأ من الشاشة رقم 0 (شاشة العملاء)
   DashboardCubit() : super(0);
 
   // دالة لتغيير الشاشة عند الضغط على القائمة الجانبية
