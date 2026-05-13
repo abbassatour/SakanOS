a979d6705a9f029868b9d279462d5a80c6757c4d
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-05-13 14:39:36 +0300

    dashboard_page.dart

--- lib/dashboard/view/dashboard_page.dart
+++ lib/dashboard/view/dashboard_page.dart
@@ -135,7 +135,7 @@ class DashboardView extends StatelessWidget {
     // ==========================================
     if (authState.hasPermission(AppPermissions.manageLegalAffairs)) {
       availableTabs.add(NavTab(
-        label: 'الأرشيف',
+        label: 'القانونية',
         icon: Icons.gavel_outlined,
         selectedIcon: Icons.gavel,
         page: const LegalAffairsPage(),
