a29715f1adb30c22500603ad22710888f7b1bcde
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-06-22 09:33:29 +0300

    buildings no issius

--- lib/buildings/widgets/apartment_details_dialog.dart
+++ lib/buildings/widgets/apartment_details_dialog.dart
@@ -1,5 +1,5 @@
 // lib/buildings/widgets/apartment_details_dialog.dart
-// ignore_for_file: always_use_package_imports, depend_on_referenced_packages, cascade_invocations
+// ignore_for_file: depend_on_referenced_packages, cascade_invocations
 
 import 'dart:async';
 import 'dart:convert';
