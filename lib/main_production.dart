ad052c4a624162d1a3dad1c7f4e1fed9cb749610
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-07-06 19:22:08 +0300

    production and staging copy of development

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,30 +1,36 @@
 // lib/main_development.dart
+import 'dart:io'; // لفحص نظام التشغيل
+import 'package:flutter/foundation.dart'; // لفحص بيئة الويب kIsWeb
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart'; 
 import 'package:our_home_erp_app/app/app.dart';
 import 'package:our_home_erp_app/bootstrap.dart';
 
 void main() async {
-  // 1. التأكد من تهيئة بيئة فلاتر والنافذة أولاً
+  // 1. التأكد من تهيئة بيئة فلاتر أولاً
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
 
-  // 2. إعدادات النافذة الافتراضية
-  WindowOptions windowOptions = const WindowOptions(
-    title: 'نظام بيتنا العقاري', 
-    center: true,
-    minimumSize: Size(800, 600), // يفضل وضع حجم أدنى لكي لا يصغر الموظف الشاشة جداً وتختفي الأزرار
-  );
-  
-  // 3. التحكم بالنافذة قبل إظهارها للمستخدم
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
+  // 2. 🛡️ حماية الأندرويد: تشغيل إعدادات النافذة فقط إذا كنا على أنظمة الكمبيوتر
+  if (!kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux)) {
+    await windowManager.ensureInitialized();
+
+    // إعدادات النافذة الافتراضية للكمبيوتر
+    WindowOptions windowOptions = const WindowOptions(
+      title: 'نظام بيتنا العقاري', 
+      center: true,
+      minimumSize: Size(800, 600), // يفضل وضع حجم أدنى لكي لا يصغر الموظف الشاشة جداً وتختفي الأزرار
+    );
     
-    // 🌟 السطر السحري الجديد: تكبير النافذة لتأخذ كامل الشاشة (Maximized) تلقائياً
-    await windowManager.maximize(); 
-  });
+    // التحكم بالنافذة قبل إظهارها للمستخدم
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+      
+      // 🌟 السطر السحري الجديد: تكبير النافذة لتأخذ كامل الشاشة (Maximized) تلقائياً
+      await windowManager.maximize(); 
+    });
+  }
 
-  // 4. تشغيل التطبيق
+  // 3. تشغيل التطبيق (هذا السطر سيعمل للكمبيوتر والأندرويد بشكل طبيعي)
   bootstrap((erpRepository) => App(erpRepository: erpRepository));
 }
