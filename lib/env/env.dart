482a4599bf5e7b8af22a94554b369cf9955ea686
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-07-07 18:38:07 +0300

    api key secured

---
+++ lib/env/env.dart
@@ -0,0 +1,14 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  // استخدام obfuscate: true سيقوم بتشفير المفتاح داخل الكود
+  @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
+  static final String supabaseUrl = _Env.supabaseUrl;
+
+  @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
+  static final String supabaseAnonKey = _Env.supabaseAnonKey;
+}
