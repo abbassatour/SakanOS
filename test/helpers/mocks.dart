9566c335f07c46632fa656ac17bad53df657d2bb
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-07-07 17:55:27 +0300

    payment ui and logic have test files

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,6 +1,15 @@
 // test/helpers/mocks.dart
+import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:erp_repository/erp_repository.dart';
+import 'package:our_home_erp_app/auth/cubit/auth_cubit.dart';
+import 'package:our_home_erp_app/payments/cubit/payments_cubit.dart';
 
-// 🌟 Mock مركزي لمستودع البيانات للتحكم بردوده أثناء الاختبارات
+// 🌟 Mock لمستودع البيانات
 class MockErpRepository extends Mock implements ErpRepository {}
+
+// 🌟 Mocks لإدارة الحالة (Cubits) لكي نخدع الواجهة بأن البيانات جاهزة
+class MockAuthCubit extends MockCubit<AuthState> implements AuthCubit {}
+
+class MockPaymentsCubit extends MockCubit<PaymentsState>
+    implements PaymentsCubit {}
